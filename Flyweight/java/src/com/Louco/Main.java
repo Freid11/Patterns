bd3809b306d84243b5eeeaad8a4587a75235ed77
Louco <dor2014spb@gmail.com>
2017-03-27 16:20:47 +0300

    c# and java FlyWeight

---
+++ Flyweight/java/src/com/Louco/Main.java
@@ -0,0 +1,73 @@
+package com.Louco;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ShapeFactory shapeFactory = new ShapeFactory();
+        List<Shape> shapes = new ArrayList<>();
+        shapes.add(shapeFactory.gerShape("Круг"));
+        shapes.add(shapeFactory.gerShape("Точка"));
+        shapes.add(shapeFactory.gerShape("Точка"));
+        shapes.add(shapeFactory.gerShape("Квадрат"));
+        shapes.add(shapeFactory.gerShape("Точка"));
+        shapes.add(shapeFactory.gerShape("Круг"));
+
+        Random ran = new Random();
+        for (Shape shape: shapes){
+            int x = ran.nextInt(100);
+            int y = ran.nextInt(100);
+            shape.draw(x,y);
+        }
+    }
+}
+
+interface Shape{
+    void draw(int x, int y);
+}
+
+class Point implements Shape{
+    int r = 5;
+    public void draw(int x, int y){
+        System.out.println("Точка");
+    }
+}
+
+class Circle implements Shape{
+    int a =10;
+    public void draw(int x, int y){
+        System.out.println("Круг");
+    }
+}
+
+class Square implements Shape{
+    public void draw(int x, int y){
+        System.out.println("Квадрат");
+    }
+}
+
+class ShapeFactory{
+    private static final Map<String, Shape> shapes = new HashMap<>();
+    public Shape gerShape(String shapename){
+        Shape shape = shapes.get(shapename);
+        if(shape == null){
+            switch (shapename){
+                case "Точка":
+                    shape = new Point();
+                    break;
+
+                case "Круг":
+                    shape = new Circle();
+                    break;
+                case "Квадрат":
+                    shape = new Square();
+                    break;
+            }
+            shapes.put(shapename, shape);
+        }
+        return shape;
+    }
+}
+
+
